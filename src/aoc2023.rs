03b80f904c813398d0e73deaa1cd0baeb51fc3d8
johnjor <johnjor@cisco.com>
2023-12-05 15:54:58 -0500

    Day 5 part 1 done!

--- src/aoc2023.rs
+++ src/aoc2023.rs
@@ -426,4 +426,154 @@ pub mod aoc2023 {
         let sum: i32 = card_copies.into_values().sum();
         println!("{}", sum);
     }
+
+    // Day 5
+    fn process_rules(input: i64, rules: &Vec<Vec<i64>>) -> i64 {
+        for rule in rules {
+            if input >= rule[1] && input < rule[1] + rule[2] {
+                return input - rule[1] + rule[0];
+            }
+        }
+        return input;
+    }
+
+    pub fn day5part1(mut lines: Lines<BufReader<File>>) {
+        let binding = lines.next().unwrap().unwrap();
+        let seeds: Vec<i64> = binding.as_str()
+            .split(':').nth(1).unwrap().trim().split(' ')
+            .map(|x| { x.parse::<i64>().unwrap() })
+            .collect();
+
+        let mut seed_to_soil: Vec<Vec<i64>> = vec![];
+        let mut soil_to_fertilizer: Vec<Vec<i64>> = vec![];
+        let mut fertilizer_to_water: Vec<Vec<i64>> = vec![];
+        let mut water_to_light: Vec<Vec<i64>> = vec![];
+        let mut light_to_temperature: Vec<Vec<i64>> = vec![];
+        let mut temperature_to_humidity: Vec<Vec<i64>> = vec![];
+        let mut humidity_to_location: Vec<Vec<i64>> = vec![];
+
+        let mut collect_seed_to_soil = false;
+        let mut collect_soil_to_fertilizer = false;
+        let mut collect_fertilizer_to_water = false;
+        let mut collect_water_to_light = false;
+        let mut collect_light_to_temperature = false;
+        let mut collect_temperature_to_humidity = false;
+        let mut collect_humidity_to_location = false;
+
+        for line_result in lines {
+
+            if let Ok(line) = line_result {
+                if line == "seed-to-soil map:" {
+                    collect_seed_to_soil = true;
+                    continue
+                } else if line == "soil-to-fertilizer map:" {
+                    collect_soil_to_fertilizer = true;
+                    continue
+                } else if line == "fertilizer-to-water map:" {
+                    collect_fertilizer_to_water = true;
+                    continue
+                } else if line == "water-to-light map:" {
+                    collect_water_to_light = true;
+                    continue
+                } else if line == "light-to-temperature map:" {
+                    collect_light_to_temperature = true;
+                    continue
+                } else if line == "temperature-to-humidity map:" {
+                    collect_temperature_to_humidity = true;
+                    continue
+                } else if line == "humidity-to-location map:" {
+                    collect_humidity_to_location = true;
+                    continue
+                } else if line == "" {
+                    collect_seed_to_soil = false;
+                    collect_soil_to_fertilizer = false;
+                    collect_fertilizer_to_water = false;
+                    collect_water_to_light = false;
+                    collect_light_to_temperature = false;
+                    collect_temperature_to_humidity = false;
+                    collect_humidity_to_location = false;
+                    continue
+                }
+
+                if collect_seed_to_soil {
+                    let temp = line.as_str().split(' ')
+                        .map(|x| { x.parse::<i64>().unwrap() })
+                        .collect();
+                    seed_to_soil.push(temp);
+                }
+
+                if collect_soil_to_fertilizer {
+                    let temp = line.as_str().split(' ')
+                        .map(|x| { x.parse::<i64>().unwrap() })
+                        .collect();
+                    soil_to_fertilizer.push(temp);
+                }
+
+                if collect_fertilizer_to_water {
+                    let temp = line.as_str().split(' ')
+                        .map(|x| { x.parse::<i64>().unwrap() })
+                        .collect();
+                    fertilizer_to_water.push(temp);
+                }
+
+                if collect_water_to_light {
+                    let temp = line.as_str().split(' ')
+                        .map(|x| { x.parse::<i64>().unwrap() })
+                        .collect();
+                    water_to_light.push(temp);
+                }
+
+                if collect_light_to_temperature {
+                    let temp = line.as_str().split(' ')
+                        .map(|x| { x.parse::<i64>().unwrap() })
+                        .collect();
+                    light_to_temperature.push(temp);
+                }
+
+                if collect_temperature_to_humidity {
+                    let temp = line.as_str().split(' ')
+                        .map(|x| { x.parse::<i64>().unwrap() })
+                        .collect();
+                    temperature_to_humidity.push(temp);
+                }
+
+                if collect_humidity_to_location {
+                    let temp = line.as_str().split(' ')
+                        .map(|x| { x.parse::<i64>().unwrap() })
+                        .collect();
+                    humidity_to_location.push(temp);
+                }
+            }
+        }
+
+        let soil: Vec<i64> = seeds.into_iter().map(|x| {
+            process_rules(x, &seed_to_soil)
+        }).collect();
+
+        let fertilizer: Vec<i64> = soil.into_iter().map(|x| {
+            process_rules(x, &soil_to_fertilizer)
+        }).collect();
+
+        let water: Vec<i64> = fertilizer.into_iter().map(|x| {
+            process_rules(x, &fertilizer_to_water)
+        }).collect();
+
+        let light: Vec<i64> = water.into_iter().map(|x| {
+            process_rules(x, &water_to_light)
+        }).collect();
+
+        let temperature: Vec<i64> = light.into_iter().map(|x| {
+            process_rules(x, &light_to_temperature)
+        }).collect();
+
+        let humidity: Vec<i64> = temperature.into_iter().map(|x| {
+            process_rules(x, &temperature_to_humidity)
+        }).collect();
+
+        let location: Vec<i64> = humidity.into_iter().map(|x| {
+            process_rules(x, &humidity_to_location)
+        }).collect();
+
+        println!("{}", location.into_iter().min().unwrap())
+    }
 }
